9627d25b445471436c441ce6c3e483297596449a
gualtierigio <gualtierigio@gmail.com>
2021-09-15 16:37:12 +0200

    inizio con l inserimento delle img

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      hobbits: [
+        {name: 'Bilbo',
+         surname: 'Baggins',
+         sourceImg: 'img/bilbo.jpg'
+        },
+        {
+          name: 'Samwise',
+          surname: 'Gamgee',
+          sourceImg: 'img/Samwise_the_Brave.jpg'
+        },
+        {name: 'Frodo',
+         surname: 'Baggins',
+         sourceImg: 'img/frodo.jpg'
+        },
+        {
+          name: 'Pipino',
+          surname: 'Peregrino Tuc',
+          sourceImg: 'img/pipino-tuc.jpg'
+        },
+        {
+          name: 'Merry',
+          surname: 'Peregrino Tuc',
+          sourceImg: 'img/merry.jpg'
+        },
+        {name: 'Smigol',
+         surname: 'Unknown',
+         sourceImg: 'img/smigol.jpg'
+        },
+        {name: 'Gollum',
+         surname: 'Unknown',
+         sourceImg: 'img/gollum.jpg'
+        },
+      ]
     }
 });
